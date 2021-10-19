a637c295065f8c349082a5a7018644310bc16a27
ashutosh <ashutoshanand2560@gmail.com>
2021-10-19 10:47:32 +0530

    commit

--- bankbot/settings.py
+++ bankbot/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
